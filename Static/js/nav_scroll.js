31fdd9bff111b67ee06ea6dfb5a6e408121f0d78
Sankalp Mukim <sankalpmukim@gmail.com>
2023-04-04 19:04:37 +0530

    kaif work

---
+++ Static/js/nav_scroll.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     document.getElementsByTagName("header").style.backgroundColor = "transparent";
+//   } else {
+//     document.getElementsByTagName("header").style.backgroundColor = "white";
+//   }
+}
