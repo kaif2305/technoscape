9c7d08162b563bc1561796bd275a1ed73af89917
Sankalp Mukim <sankalpmukim@gmail.com>
2023-04-07 12:24:03 +0530

    remove gsap effect on small screen

--- Static/js/partner_slide.js
+++ Static/js/partner_slide.js
@@ -3,14 +3,37 @@ gsap.registerPlugin(ScrollTrigger);
 const sections = gsap.utils.toArray(".panel"),
   container = document.querySelector(".academic-partners");
 
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1.3),
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".academic-partners",
-    pin: true,
-    scrub: 1,
-    snap: 2 / (sections.length - 1),
-    end: () => "+=" + container.offsetWidth,
-  },
-});
+let scrollTriggerInstance;
+
+function initGsapEffect() {
+  scrollTriggerInstance = gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1.3),
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".academic-partners",
+      pin: true,
+      scrub: 1,
+      snap: 2 / (sections.length - 1),
+      end: () => "+=" + container.offsetWidth,
+    },
+  });
+}
+
+function toggleGsapEffect() {
+  const windowWidth = window.innerWidth;
+
+  if (windowWidth < 768) {
+    if (scrollTriggerInstance) {
+      scrollTriggerInstance.scrollTrigger.kill();
+      scrollTriggerInstance.kill();
+      scrollTriggerInstance = null;
+    }
+  } else {
+    if (!scrollTriggerInstance) {
+      initGsapEffect();
+    }
+  }
+}
+
+toggleGsapEffect(); // Call once on page load
+window.addEventListener("resize", toggleGsapEffect);
